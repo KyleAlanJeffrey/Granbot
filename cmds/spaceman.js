660f9e903531b3d8309c4c90b4e117df44835cb6
Business Kyle <kajeffre@ucsc.edu>
2020-07-02 13:47:38 -0700

    added remove features

--- cmds/spaceman.js
+++ cmds/spaceman.js
@@ -1,24 +1,48 @@
 const Discord = require('discord.js');
-
-let spaceman = {
-    muteAll: muteAll,
+let variables = {
+    running: false,
+    stopMsgId: undefined,
+    members: undefined,
+    spacemanRole: undefined,
 };
 
-function muteAll(msgObj) {
+function stop(user) {
+    console.log(`${user.username} stopped the game.`);
+    // Remove Rolls and unmute everyone;
+    variables.members.each((member) => {
+        member.voice.setMute(false);
+        member.roles.remove(variables.spacemanRole);
+    });
+    variables.running = false;
+}
+function run(msgObj) {
+    console.log(`${msgObj.member.user.username} started a game of Spaceman`);
     const spacemanRole = makeSpacemanRole(msgObj);
-    const guild = msgObj.guild;
+    makeSpacemen(msgObj, spacemanRole, variables);
+    const message_embed = new Discord.MessageEmbed()
+        .setColor('#C4C9BF')
+        .setTitle('Now Playing a round of Unfortunate Spaceman')
+        .setDescription('Who is the monster? Its Probably Mitchell.')
+        .addFields(
+            { name: '---------------------------------------', value: '\u200B' },
+            { name: 'Thumbs up this stop sign to end the round', value: '\u200B' },
+        )
+        .setImage('https://upload.wikimedia.org/wikipedia/commons/8/81/Stop_sign.png');
+    msgObj.channel.send(message_embed).then(msg => { variables.stopMsgId = msg.id; });
+    variables.running = true;
+    variables.spacemanRole = spacemanRole;
+}
+
+function makeSpacemen(msgObj, spacemanRole) {
     // Iterate through the members in the channel the message came from and give spaceman role
-    // const spacemanRole = guild.roles.cache.find(role => role.name === 'Unfortunate Spaceman');
     const voice_channel = msgObj.member.voice.channel;
     const members = voice_channel.members;
+    variables.members = members;
     members.each(member => {
-        member.setNickname('Spaceman').catch(err => { console.log(`Setting Nickname Error: ${err}`) });
-        member.voice.setMute(true);
-        member.roles.add(spacemanRole).catch(err => { console.log(`Setting Role Error: ${(err)}`) });
-        console.log('Muted & added role to user: ' + member.displayName);
+        member.voice.setMute(true).catch(rej => { console.log(`Muting Error: ${(rej)}`); }); // Have to mute before changing role
+        member.setNickname('Spaceman').catch(err => { console.log(`Setting Nickname Error: ${err}`); }); // change nickname
+        member.roles.add(spacemanRole).catch(err => { console.log(`Setting Role Error: ${(err)}`); }); // give spaceman role
     });
-    let message_embed = new Discord.MessageEmbed();
-
 }
 function makeSpacemanRole(msgObj) {
     // Make the spaceman role which allows people to unumte themselves
@@ -40,4 +64,10 @@ function makeSpacemanRole(msgObj) {
     }
     return spaceman_role;
 }
+
+const spaceman = {
+    run: run,
+    stop: stop,
+    vars: variables,
+};
 module.exports = spaceman;
--- index.js
+++ index.js
@@ -5,22 +5,25 @@ const spaceman = require('./cmds/spaceman');
 const { prefix, token } = require('./bot-config.json');
 const fs = require('fs');
 
+
+/* -------------------------------------- */
+
+/* -------------------------------------- */
 const bot = new Discord.Client();
-
 bot.login(token);
-
-//  bot.on() is where all commands are done thorugh  the bot. This instance is run when the bot turns on
 bot.on('ready', () => {
     console.log('Bot is online!');
 });
-
-// This logs the bbot in with a token from the developer page
 bot.on('message', (msg) => {
     if (msg.content.startsWith(prefix)) {
         parseCommand(msg);
     }
 });
-
+bot.on('messageReactionAdd', (reaction, user) => {
+    if (spaceman.vars.running && (reaction.message.id == spaceman.vars.stopMsgId)) {
+        spaceman.stop(reaction, user);
+    }
+});
 function parseCommand(msgObj) {
     let message = msgObj.content.slice(1);
     const messageArr = message.split(/ (.*)/);
@@ -28,23 +31,33 @@ function parseCommand(msgObj) {
     message = messageArr[1];
 
     if (command === 'spaceman') {
-        spaceman.muteAll(msgObj, Discord);
-    } else if (command === 'uwu') {
+        spaceman.run(msgObj);
+    }
+    else if (command === 'uwu') {
         uwuifying.custom(message, msgObj, uwuifying_data, Discord);
 
-    } else {
+    }
+    else if (command === 'commands') {
+        msgObj.channel.send(commandsEmbed);
+    }
+    else {
         msgObj.reply('Not a command!');
     }
 }
 
-// async function eh() {
-//     return await new Promise((acc, rej) => {
-//         let work = 7;
-//         if (work > 5) {
-//             acc("successful promisee");
-//         } else {
-//             rej("rejected promise");
-//         }
-//     });
-// }
-// eh().then(res => console.log(res)).catch(err => console.log(err));
+let commandsEmbed = new Discord.MessageEmbed()
+    .setColor('#7FFFBF')
+    .setTitle('Granbot Commands')
+    // .setURL('https://discord.js.org/')
+    // .setAuthor('Some name', 'https://i.imgur.com/wSTFkRM.png', 'https://discord.js.org')
+    // .setDescription('Some description here')
+    .setThumbnail('https://i1.wp.com/metro.co.uk/wp-content/uploads/2019/02/sei_53672523-1e20.jpg?quality=90&strip=all&zoom=1&resize=540%2C699&ssl=1')
+    .addFields(
+        { name: '---------------------------------------', value: '\u200B' },
+        { name: `${prefix}uwuify`, value: 'Translates following text from plain english into weaboo, or from weaboo to super weaboo' },
+        { name: `${prefix}spaceman`, value: 'For use when starting a game of Unfortunate Spaceman' },
+    );
+    // .addField('Inline field title', 'Some value here', true)
+    // .setImage('https://i1.wp.com/metro.co.uk/wp-content/uploads/2019/02/sei_53672523-1e20.jpg?quality=90&strip=all&zoom=1&resize=540%2C699&ssl=1');
+	// .setTimestamp()
+	// .setFooter('Some footer text here', 'https://i.imgur.com/wSTFkRM.png');
